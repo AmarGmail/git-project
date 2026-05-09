db01c60fddc09389c0f1fe4cbaf92c11dbd08727
Amarjyoti Lahkar <amarjyotilahkar37@gmail.com>
2026-05-09 08:28:16 +0000

    gettings functionality added

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ print("Welcome, you are in git project")
 def add(a, b):
     return a + b
 
+def greet(name):
+    return f"Hello, {name}!"
+
+greet("John")
+
 add_result = add(70, 30)
 print("The result of addition is:", add_result)
 
